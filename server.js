c8e84fe37d2ffe0ab231ad8a14854570e16d8145
Jason Tom <jtom77@gmail.com>
2017-04-27 23:35:59 -0700

    Added working ebay

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var request = require('request');
+var firebase = require("firebase");
 
 var ebayID = 'JasonTom-Jamazon-PRD-82466ad0e-648f6631';
-var url = `http://open.api.ebay.com/shopping?callname=FindPopularItems&responseencoding=JSON&appid=${ebayID}&siteid=0&version=713&QueryKeywords=`;
-var port = '3030';
+var walmartKey = '2a5cbmcqb4wn56wbfhfe37e5'
+var eBayUrl = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${ebayID}&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US&siteid=0&keywords=`;
+var walmartUrl = `http://api.walmartlabs.com/v1/search?apiKey=${walmartKey}&query=`;
+var port = '8000';
+
+var config = {
+    apiKey: "AIzaSyBUEK9ezt4FcUQo_hyWkCJ3D5IHEgEvMdo",
+    authDomain: "jamazon-578c7.firebaseapp.com",
+    databaseURL: "https://jamazon-578c7.firebaseio.com",
+    projectId: "jamazon-578c7",
+    storageBucket: "jamazon-578c7.appspot.com",
+    messagingSenderId: "973631780135"
+  };
 
 app.get('/ebay/search/:query', function(req, res) {
-  request(url + req.params.query, function (error, response, body) {
+  request(eBayUrl + req.params.query, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.header('Access-Control-Allow-Origin', '*');
       res.send(body);
@@ -16,6 +28,13 @@ app.get('/ebay/search/:query', function(req, res) {
   })
 
 });
+
+app.get('/walmart/search/:query', function(req, res) {
+  request(walmartUrl + req.params.query, function(error, response, body) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(body);
+  });
+})
 
 // we'll create our routes here
 
