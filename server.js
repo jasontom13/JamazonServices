d8e9eb766341a939d53b584e731e0076e1f0e57c
Jason Tom <jtom77@gmail.com>
2020-04-04 13:36:56 -0700

    improvements

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ var router = express.Router();
 var request = require('request');
 var firebase = require("firebase");
 
-var ebayID = 'JasonTom-Jamazon-PRD-82466ad0e-648f6631';
-var walmartKey = '2a5cbmcqb4wn56wbfhfe37e5'
+var ebayID = ''
+var walmartKey = ''
 var eBayUrl = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${ebayID}&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US&siteid=0&keywords=`;
 var walmartUrl = `http://api.walmartlabs.com/v1/search?apiKey=${walmartKey}&query=`;
 
 var config = {
-    apiKey: "AIzaSyBUEK9ezt4FcUQo_hyWkCJ3D5IHEgEvMdo",
-    authDomain: "jamazon-578c7.firebaseapp.com",
-    databaseURL: "https://jamazon-578c7.firebaseio.com",
-    projectId: "jamazon-578c7",
-    storageBucket: "jamazon-578c7.appspot.com",
-    messagingSenderId: "973631780135"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
 app.get('/Ebay/search/:query', function(req, res) {
